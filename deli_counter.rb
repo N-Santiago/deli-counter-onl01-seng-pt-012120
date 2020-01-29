b9af91e014002624c2dd770b0d8a62a5a615fdff
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-29 05:47:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,13 +19,11 @@ def take_a_number(array, name)
   return name, position 
 end
 
-def now_serving(array)
-  if array.empty? == true 
-    puts "There is nobody waiting to be served!"
-  elsif array.empty? == false 
-    puts "Currently serving #{array.shift}."
-  end 
-end 
-
-now_serving(katz_deli)
+now_serving(array)
+if array.empty? == true
+  puts "There is nobody waiting to be served!"
+elsif array.empty? == false 
+puts "Currently serving #{array.shift}."
+  end
+end
   
