1b46618b9946c47be540d66571a133082e486a3b
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-29 05:09:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ if array.length >= 1
   array.each |name|
   newarray.push("#{name}, #{counter}")
 end
-puts "The line is currently: #{nuarray.join(" ")}"
+puts "The line is currently: #{newarray.join(" ")}"
 else 
   puts "The line is currently empty."
 end
