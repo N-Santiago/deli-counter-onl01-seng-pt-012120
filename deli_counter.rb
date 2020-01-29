2220fea9eaa03fc54c03f556785279dce0f2ba10
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-29 05:17:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,4 +5,9 @@ def line(array)
     puts "The line is currently empty."
   else 
     message = "The line is currently:"
+array.each_with_index do |value, index| 
+    message += " #{index.to_i+1}. #{value}"
+  end 
+  puts "#{message}"
+  end 
 end 
