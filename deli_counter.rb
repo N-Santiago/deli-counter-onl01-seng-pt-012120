b33a3cc55c9e59da5c19fbb9ae3d8554aaa52a11
Norberto Santiago <norbertosantiago82@gmail.com>
2020-01-29 05:40:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -27,6 +27,6 @@ now_serving(array)
 if array.empty == true
   puts "There is nobody waiting to be served!"
 elsif array.empty == false 
-puts ""
-  
+puts "Currently serving #{array.shift}."
+end
   
